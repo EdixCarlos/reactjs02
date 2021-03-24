31ac162ed53d977b5c33d3106162a81997f59b3c
Edix Carlos <edix.carlos@tecsup.edu.pe>
2021-03-24 12:29:11 -0500

    Quinto Commit

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,17 +1,12 @@
+import Part1 from './parts/Part1' 
+import Part2 from './parts/Part2' 
+import Part3 from './parts/Part3' 
 const Content = (props) => {
-    
-  
     return (
       <div>
-        <p>
-          {props.part1} {props.exercises1}
-        </p>
-        <p>
-          {props.part2} {props.exercises2}
-        </p>
-        <p>
-          {props.part3} {props.exercises3}
-        </p>
+        <Part1 part1={props.part1} exercises1={props.exercises1}/>
+        <Part2 part2={props.part2} exercises2={props.exercises2}/>
+        <Part3 part3={props.part3} exercises3={props.exercises3}/>
       </div>
     )
   }
